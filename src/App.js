63a1b6f99578fb94b77240181aba15059dc68c50
zhaojinye <zhaojinye@cmcm.com>
2018-01-09 12:13:24 +0800

    commit react static components

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Header from './components/Header';
+import Home from './components/Home';
+
 class App extends Component {
   render() {
     return (
       <div className="container">
         <div className="row">
           <div className="col-xs-1 col-xs-offset-11">
-            <h1>hello world</h1>
+            <Home />
           </div>
-
         </div>
+
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <h1>Hello world</h1>
+          </div>
+        </div>
+      
+        <div className="row">
+          <div className="col-xs-1 col-xs-offset-11">
+            <Header />
+          </div>
+        </div>
+
       </div>
     );
   }
